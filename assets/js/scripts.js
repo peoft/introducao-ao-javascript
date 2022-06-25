c7656cebd8d372525b23c8beadddd2e9bba1d0fe
peoft <paulo.ostermann.filho@gmail.com>
2022-06-25 19:56:39 -0300

    Usando addEventListener

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,5 +1,10 @@
 var currentNumberWrapper = document.getElementById('currentNumber');
 var currentNumber = 0;
+var incrementButton = document.getElementById('increment');
+var decrementButton = document.getElementById('decrement');
+
+incrementButton.addEventListener("click", increment);
+decrementButton.addEventListener("click", decrement);
 
 function increment() {
     currentNumber++;
